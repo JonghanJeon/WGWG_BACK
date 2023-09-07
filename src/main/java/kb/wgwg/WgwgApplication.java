dfe2b0e215ed83748e3db5f0a94aeb92d5c9c825
BullChallenger <am23am45@naver.com>
2023-09-07 16:27:47 +0900

    Init Commit

---
+++ src/main/java/kb/wgwg/WgwgApplication.java
@@ -0,0 +1,13 @@
+package kb.wgwg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgwgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WgwgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kb/wgwg/WgwgApplicationTests.java
@@ -0,0 +1,13 @@
+package kb.wgwg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WgwgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
