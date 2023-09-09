f30d6a9b8bfff1114b656c0185bc378bdf3ad593
Kyongryul Na <skrudfbf0813@naver.com>
2023-09-09 14:29:54 +0900

    Update FEAT Login delete
    Update FEAT Login delete

--- src/main/java/kb/wgwg/repository/UserRepository.java
+++ src/main/java/kb/wgwg/repository/UserRepository.java
@@ -2,7 +2,9 @@ package kb.wgwg.repository;
 
 import kb.wgwg.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    @Modifying
+    void deleteByUserSeq(Long userSeq);
 }
