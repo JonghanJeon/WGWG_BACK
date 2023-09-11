d971aa4f9a81d18a355cab7139a160c0b6694434
HwangHyeryeong <hrh4225@gmail.com>
2023-09-11 14:59:42 +0900

    [FEAT] 댓글 수정 기능

--- src/main/java/kb/wgwg/repository/CommentRepository.java
+++ src/main/java/kb/wgwg/repository/CommentRepository.java
@@ -2,7 +2,11 @@ package kb.wgwg.repository;
 
 import kb.wgwg.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
+    @Modifying
+    @Query("UPDATE Comment c set c.content = :content where c.commentSeq = :commentSeq")
+    int updateCommentByCommentSeq(Long commentSeq, String content);
 }
