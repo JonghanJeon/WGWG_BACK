3362c784ac3e20714a0b18ef47361e4633e304aa
Kyongryul Na <skrudfbf0813@naver.com>
2023-09-11 17:11:15 +0900

    [FEAT] Comment 등록 추가

--- src/main/java/kb/wgwg/dto/CommentDTO.java
+++ src/main/java/kb/wgwg/dto/CommentDTO.java
@@ -1,8 +1,39 @@
 package kb.wgwg.dto;
 
+import kb.wgwg.domain.User;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 public class CommentDTO {
+    @Getter
+    @Setter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
+    @Builder
+    public static final class CommentInsertRequestDTO {
+        private Long userSeq;
+        private Long articleSeq;
+        private String content;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static final class CommentInsertResponseDTO {
+        private String content;
+        private String writer;
+        private LocalDateTime insertDate;
+        private LocalDateTime updateDate;
+
+        @Builder
+        public CommentInsertResponseDTO(String content, User writer, LocalDateTime insertDate, LocalDateTime updateDate) {
+            this.content = content;
+            this.writer = writer.getNickName();
+            this.insertDate = insertDate;
+            this.updateDate = updateDate;
+        }
+    }
     @Setter
     @Getter
     @NoArgsConstructor
--- src/main/java/kb/wgwg/service/CommentService.java
+++ src/main/java/kb/wgwg/service/CommentService.java
@@ -1,30 +1,61 @@
 package kb.wgwg.service;
 
+import kb.wgwg.domain.Article;
 import kb.wgwg.domain.Comment;
+import kb.wgwg.domain.User;
 import kb.wgwg.dto.CommentDTO.*;
+import kb.wgwg.repository.ArticleRepository;
 import kb.wgwg.repository.CommentRepository;
+import kb.wgwg.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityNotFoundException;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class CommentService {
+
     private final ModelMapper modelMapper;
     private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final ArticleRepository articleRepository;
+    private final EntityManager entityManager;
 
-    public int updateComment(CommentUpdateDTO dto){
+    public CommentInsertResponseDTO insertComment(CommentInsertRequestDTO dto) {
+        User user = userRepository.findById(dto.getUserSeq()).orElseThrow(
+                () -> new EntityNotFoundException("해당 사용자를 찾을 수 없습니다.")
+        );
+
+        Article article = articleRepository.findById(dto.getArticleSeq()).orElseThrow(
+                () -> new EntityNotFoundException("해당 게시글을 찾을 수 없습니다.")
+        );
+
+        Comment comment = Comment.builder()
+                .content(dto.getContent())
+                .writer(user)
+                .parentArticle(article).build();
+
+        Comment savedComment = commentRepository.save(comment);
+        entityManager.flush();
+        CommentInsertResponseDTO result = modelMapper.map(savedComment, CommentInsertResponseDTO.class);
+        return result;
+    }
+
+    public int updateComment(CommentUpdateDTO dto) {
         Comment comment = commentRepository.findById(dto.getCommentSeq()).orElseThrow(
                 () -> new EntityNotFoundException("해당 댓글을 찾을 수 없습니다.")
         );
 
         return commentRepository.updateCommentByCommentSeq(dto.getCommentSeq(), dto.getContent());
     }
+
     public int deleteComment(Long id){
         return commentRepository.deleteByCommentSeq(id);
     }
+
 }
