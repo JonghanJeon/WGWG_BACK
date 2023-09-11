1a502e199e2389c36d085c32be067992932814e1
Kyongryul Na <skrudfbf0813@naver.com>
2023-09-11 13:59:08 +0900

    [FEAT] Article 기본로직 추가

---
+++ src/main/java/kb/wgwg/controller/CommentController.java
@@ -0,0 +1,11 @@
+package kb.wgwg.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/comments")
+public class CommentController {
+}
---
+++ src/main/java/kb/wgwg/dto/CommentDTO.java
@@ -0,0 +1,5 @@
+package kb.wgwg.dto;
+
+public class CommentDTO {
+
+}
