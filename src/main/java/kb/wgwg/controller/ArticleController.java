c090d7352c35cd800eb669fe3af05ea3067c51a4
Kyongryul Na <skrudfbf0813@naver.com>
2023-09-14 16:44:40 +0900

    [FEAT] Article 해당 유저만 페이징 처리

--- src/main/java/kb/wgwg/controller/ArticleController.java
+++ src/main/java/kb/wgwg/controller/ArticleController.java
@@ -3,6 +3,7 @@ package kb.wgwg.controller;
 import kb.wgwg.common.ResponseMessage;
 import kb.wgwg.common.StatusCode;
 import kb.wgwg.dto.ArticleDTO.*;
+import kb.wgwg.dto.BankingDTO.*;
 import kb.wgwg.dto.BaseResponseDTO;
 import kb.wgwg.service.ArticleService;
 import lombok.RequiredArgsConstructor;
@@ -117,6 +118,28 @@ public class ArticleController {
             response.setSuccess(false);
         }
 
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping(value = "/read/my/{userSeq}")
+    public ResponseEntity<BaseResponseDTO> readArticleByUserSeq(@PathVariable Long userSeq, @PageableDefault(size = 10) Pageable pageable) {
+        BaseResponseDTO<Page<ArticleListUserResponseDTO>> response = new BaseResponseDTO<>();
+        try {
+            Page<ArticleListUserResponseDTO> result = service.findArticleByUser(userSeq, pageable);
+            response.setMessage("성공적으로 게시글 내역을 불러왔습니다.");
+            response.setStatus(200);
+            response.setSuccess(true);
+            response.setData(result);
+        } catch (EntityNotFoundException e) {
+            response.setMessage(e.getMessage());
+            response.setStatus(400);
+            response.setSuccess(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setMessage(INTERNAL_SERVER_ERROR);
+            response.setStatus(500);
+            response.setSuccess(false);
+        }
         return ResponseEntity.ok(response);
     }
 }
